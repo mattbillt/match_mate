881ff8bc524810c9b8fca31dc6366f704acfc807
Matt Trusson <matt.trusson@hotmail.com>
2023-11-29 18:28:35 -0300

    working on chat function

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
+  resources :chatrooms, only: :show
+
   get "games", to: "games#index"
   get "games/:id", to: "games#show"
 
