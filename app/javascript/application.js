aa180b16ff4036b82f10f7a2dcd1ac0ff2e6457e
camilaprates <camilaprates1@gmail.com>
2023-11-29 14:27:36 -0600

    teams-calendar-location

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,16 +1,30 @@
+// your_script.js
+
 import "@hotwired/turbo-rails";
 import "controllers";
-
-// Add the Flatpickr JS using a CDN link
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.css";
 
-// Any additional JavaScript code or imports can go here
 document.addEventListener('DOMContentLoaded', function() {
+  // Flatpickr initialization for the first date picker
   flatpickr("#datePicker1", {
     mode: "range"
   });
+
+  // Flatpickr initialization for the second date picker
   flatpickr("#datePicker2", {
     mode: "range"
+  });
+
+  // Event listener for the "Use My Location" button
+  document.getElementById('useMyLocation').addEventListener('click', function() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        // Access position.coords.latitude and position.coords.longitude
+        alert("Latitude: " + position.coords.latitude + "\nLongitude: " + position.coords.longitude);
+      });
+    } else {
+      console.log('Geolocation is not supported by this browser.');
+    }
   });
 });
