c10b592be4bc1c0f48c209ff3784dc21fc5bc97b
Ian Totti <ianptotti@gmail.com>
2023-11-30 16:30:37 -0300

    fixed errors and added list of groups to game show

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ApplicationRecord
   belongs_to :game
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
-  has_many :user_groups
+  has_many :user_groups, dependent: :destroy
   has_many :users, through: :user_groups
 end
