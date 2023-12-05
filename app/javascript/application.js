18ec77b28665de9e6e66427f1f59594234dc5ca8
camilaprates <camilaprates1@gmail.com>
2023-12-04 21:50:49 -0600

    formsignup

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,23 +6,3 @@ import "controllers";
 // import "flatpickr/dist/flatpickr.css";
 
 import "@rails/actioncable";
-
-document.addEventListener('turbo:load', function() {
-  flatpickr("#datePicker1", {
-    mode: "range"
-  });
-
-  flatpickr("#datePicker2", {
-    mode: "range"
-  });
-
-  document.getElementById('useMyLocation').addEventListener('click', function() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        alert("Latitude: " + position.coords.latitude + "\nLongitude: " + position.coords.longitude);
-      });
-    } else {
-      console.log('Geolocation is not supported by this browser.');
-    }
-  });
-});
