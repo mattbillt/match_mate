154831abc2510170c48ef3b3a160cca48a361d20
camilaprates <camilaprates1@gmail.com>
2023-12-05 16:49:08 -0600

    editprofile

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,3 +6,12 @@ import "controllers";
 // import "flatpickr/dist/flatpickr.css";
 
 import "@rails/actioncable";
+
+document.addEventListener('DOMContentLoaded', function() {
+  var btn = document.querySelector('.btn-danger');
+  var fileInput = document.querySelector('.form-control[type="file"]');
+
+  btn.addEventListener('click', function() {
+    fileInput.value = '';
+  });
+});
