e01ab6cc13f511ec6dc5029363a5334c9d80cf2c
camilaprates <camilaprates1@gmail.com>
2023-12-05 06:23:30 -0600

    loginpic

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -2,4 +2,10 @@ class SectionsController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def destroy
+    reset_session
+    cookies.delete(:user_id)
+    redirect_to root_path
+  end
 end
