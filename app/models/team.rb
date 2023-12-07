6911c73dccbb81924d4f5bef2404b67e175bbee4
Ian Totti <ianptotti@gmail.com>
2023-12-07 18:24:54 -0300

    list of upcoming games with links

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,6 +2,11 @@ class Team < ApplicationRecord
   has_many :games_as_home_team, class_name: "Game", foreign_key: "home_team_id"
   has_many :games_as_away_team, class_name: "Game", foreign_key: "away_team_id"
 
+  def upcoming_games
+    Game.where("(home_team_id = :team_id OR away_team_id = :team_id) AND date > :today", team_id: id, today: Date.today)
+  end
+
+
 
   # include PgSearch::Model
   # multisearchable against: [:name]
